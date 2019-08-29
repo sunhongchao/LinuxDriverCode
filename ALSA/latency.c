fe1c9997a128ea10601f1f5d9072be9fdf2c417e
sunchongchao <sunhongchao_2005@126.com>
2019-08-29 10:08:03 +0800

    pcm

--- ALSA/latency.c
+++ ALSA/latency.c
@@ -1,32 +1,5 @@
 
-/*
- *  Latency test program
- *
- *     Author: Jaroslav Kysela <perex@perex.cz>
- *
- *     Author of bandpass filter sweep effect:
- *         Maarten de Boer <mdeboer@iua.upf.es>
- *
- *  This small demo program can be used for measuring latency between
- *  capture and playback. This latency is measured from driver (diff when
- *  playback and capture was started). Scheduler is set to SCHED_RR.
- *
- *
- *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program; if not, write to the Free Software
- *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- *
- */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
--- ALSA/pcm_main.c
+++ ALSA/pcm_main.c
@@ -3,31 +3,46 @@
 */
 #include "inlcude/asoundlib.h"
 
-static char *device="default";
-snd_output_t *output = NULL;
-unsigned char buffer[16*1024];
+static char *device="default";  //播放音频的逻辑设备
+snd_output_t *output = NULL;        //未用
+unsigned char buffer[16*1024];    //音频数据存储缓冲区
 
 int main(void)
 {
 	int err;
 	unsigned int i;
-	snd_pcm_t *handle;
-	snd_pcm_sframes_t frames;
-	for (i = 0;i < sizeof(buffer); i++)
+	snd_pcm_t *handle;          //播放句柄，类似文件描述符fd.标识一个播放
+	snd_pcm_sframes_t frames;      //已经播放的帧数
+	for (i = 0;i < sizeof(buffer); i++)//生成播放随机数据
 		buffer[i] = random() & 0xff;
-
+	//打开播放流设备
 	if((err = snd_pcm_open(&handle,device,SND_PCM_STREAM_PLAYBACK,0)) < 0) {
 		printf("playback open error:%s\n",snd_strerror(err));
 		exit(EXIT_FAILURE);
 	}
+	/*
+		设置播放参数
+		int snd_pcm_set_params	(	snd_pcm_t * 	pcm,
+					snd_pcm_format_t 	format,
+					snd_pcm_access_t 	access, //数据存储方式，一般是交错
+					unsigned int 	channels,//播放通道数
+					unsigned int 	rate,//播放率
+					int 	soft_resample,
+					//重采样（Resampling）指的是把时间序列的频度变为另一个频度的过程。
+					把高频度的数据变为低频度叫做降采样（downsampling），把低频度变为高频度叫做增采样
+					（upsampling）。
+					unsigned int 	latency //播放延时us
+					)	
+					
+	*/
 	if((err = snd_pcm_set_params(handle,SND_PCM_FORMAT_U8,SND_PCM_ACCESS_RW_INTERLEAVED,1,48000,1,500000))<0){
 		printf("Playback open error:%s\n",snd_strerror(err));
 		exit(EXIT_FAILURE);
 	}
 	for (i = 0; i < 16; i++) {
-                frames = snd_pcm_writei(handle, buffer, sizeof(buffer));
+                frames = snd_pcm_writei(handle, buffer, sizeof(buffer));//播放
                 if (frames < 0)
-                        frames = snd_pcm_recover(handle, frames, 0);
+                        frames = snd_pcm_recover(handle, frames, 0);//从错误帧处恢复播放
                 if (frames < 0) {
                         printf("snd_pcm_writei failed: %s\n", snd_strerror(frames));
                         break;
@@ -35,7 +50,7 @@ int main(void)
                 if (frames > 0 && frames < (long)sizeof(buffer))
                         printf("Short write (expected %li, wrote %li)\n", (long)sizeof(buffer), frames);
         }
-    snd_pcm_close(handle);
+    snd_pcm_close(handle);//关闭播放流
     return 0;
 }
 
